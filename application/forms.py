27138ed83ec6116c626e0d25eb23d5a270be18ec
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-15 16:39:34 +0100

    add form data

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,11 @@ from wtforms import IntegerField, SubmitField, StringField
 from flask_wtf import FlaskForm
 
 class DetailsForm(FlaskForm):
+	film_id = IntegerField('film_id')
 	title = StringField('title')
-
+	year_released = IntegerField('year_released')
+	rating = StringField('rating')
+	genre = StringField('genre')
+	duration = IntegerField('duration')
+	
 	submit = SubmitField('Submit')
--- application/routes.py
+++ application/routes.py
@@ -1,31 +1,38 @@
 from flask import Flask, jsonify, render_template, request
 from application import app
 import requests
-#from application.forms import DetailsForm
-#from application.api_connections import ApiConnect
+from application.forms import DetailsForm
+from application.api_connections import ApiConnect
 
 print('all good')
 
-#my_api = ApiConnect()
-#print('all ok')
+my_api = ApiConnect()
+print('all ok')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	print('got here')
-	#films = my_api.get_films()
+	films = my_api.get_films()
+	error = ''
+	form = DetailsForm()
 
-	endpoint = 'https://filmflixrestapi.onrender.com/'
-	path = 'api/films'
-	url = endpoint + path
-	print(url)
-	request = requests.get(url)
-	response = request.json()
-	films = response
+	# endpoint = 'https://filmflixrestapi.onrender.com/'
+	# path = 'api/films'
+	# url = endpoint + path
+	# print(url)
+	# request = requests.get(url)
+	# response = request.json()
+	# films = response
 
-	#form = DetailsForm()
-
-	# if request.method == 'POST':
-	# 	title = form.title.data
-
-
-	return render_template('index.html', films=films)
+	if request.method == 'POST':
+	 	form_id = form.film_id.data
+		title = form.title.data
+		year_released = form.year_released.data
+		rating = form.rating.data
+		genre = form.genre.data
+		duration = form.duration.data
+		print(title)
+		error = 'test'
+		return render_template('index.html', films=films)
+		 
+	return render_template('index.html', films=films, message=error)
